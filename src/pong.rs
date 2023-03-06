dd24c30b9ac94a4ed345a332ade973a44922573c
Daniel Anugerah <danielanugerah2004@gmail.com>
2023-03-07 01:22:43 +0800

    try to pass 2st stage

--- src/pong.rs
+++ src/pong.rs
@@ -1,25 +1,24 @@
-use std::ffi::OsString;
-use std::io::{Read, read_to_string, Write};
+use std::io::{Read, Write};
 use std::net::{TcpStream};
 
 pub(crate) fn handle_ping(mut stream: TcpStream) {
     let mut buffer = [0; 10];
     stream.read(&mut buffer).unwrap();
 
-    let request_String = String::from_utf8_lossy(&buffer[..]);
+    let request_string = String::from_utf8_lossy(&buffer[..]);
 
-    println!("request_String: {:?}", request_String);
+    println!("request_string: {:?}", request_string);
 
-    if request_String == ""{
+    if request_string == ""{
         let response = b"NIL VALuE\r";
         stream.write(response).unwrap();
     }
-    else if request_String == "PING\r\n" {
+    else if request_string == "PING\r\n" {
         let response = b"PONG\r";
         stream.write(response).unwrap();
     }
     else {
-        stream.write(request_String.as_bytes()).unwrap();
+        stream.write(request_string.as_bytes()).unwrap();
     }
 
     stream.flush().unwrap();
